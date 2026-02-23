793ac80798d438c71f441d1081fab1f184fcba4d
vc7350-cloud <vc7350@gmail.com>
2026-02-23 06:07:42 -0800

    Add files via upload

---
+++ DIVISION.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("enter 1st number=");
+    scanf("%d",a&);
+    
+    printf("enter 2nd number=");
+    scanf("%d",&b);
+    
+    c=a/b;
+    printf("divison=%d",c);
+}
